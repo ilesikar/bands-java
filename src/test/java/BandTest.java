79122cc585a882d8ed6bdc2557696fd083d9a673
Ian Lesikar <ilesikar@gmail.com>
2016-05-13 11:53:47 -0700

    all_emptyAtFirst_0 passes

---
+++ src/test/java/BandTest.java
@@ -0,0 +1,26 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class BandTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Band_instantiatesCorrectly_true() {
+    Band myBand = new Band("BandName");
+    assertEquals(true, myBand instanceof Band);
+  }
+
+  @Test
+  public void getName_bandInstantiatesWithName_String() {
+    Band myBand = new Band("BandName");
+    assertEquals("BandName", myBand.getName());
+  }
+
+  @Test public void all_emptyAtFirst_0() {
+    assertEquals(0, Band.all().size());
+  }
+
+}
