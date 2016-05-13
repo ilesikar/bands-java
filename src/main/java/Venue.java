1251697dac6268045603ffadb48a7d6894eb13fc
Ian Lesikar <ilesikar@gmail.com>
2016-05-13 12:08:15 -0700

    Venue_instantiatesCorrectly_true

---
+++ src/main/java/Venue.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Venue {
+  private int id;
+  private String name;
+
+  public Venue(String name) {
+    this.name = name;
+  }
+
+}
--- src/test/java/VenueTest.java
+++ src/test/java/VenueTest.java
@@ -1,9 +1,17 @@
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Arrays;
 
 public class VenueTest {
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Venue_instantiatesCorrectly_true() {
+    Venue myVenue = new Venue("Venue1");
+    assertEquals(true, myVenue instanceof Venue);
+  }
 
 }
