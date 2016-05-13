a386e7f19a8935b3cd658c48bc36e19cb9b2c375
Ian Lesikar <ilesikar@gmail.com>
2016-05-13 10:56:10 -0700

    DatabaseRule added

--- src/test/java/ServerRule.java
+++ src/test/java/ServerRule.java
@@ -5,8 +5,7 @@ public class ServerRule extends ExternalResource {
 
   protected void before() {
     String[] args = {};
-    App.main(args); // for other apps, replace LeapYear with the name of the main app class (often App)
-   }
+    App.main(args); // for other apps, replace App with main class
 
   protected void after() {
     Spark.stop();
